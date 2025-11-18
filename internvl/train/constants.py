0a328d634d1418977caa6ac5c111eb3bf6d31471
Thillai Chithambaram <thillaichithambaram.a@gmail.com>
2025-11-18 18:22:26 -0500

    docs: add module docstring and organize constants with comments

--- internvl/train/constants.py
+++ internvl/train/constants.py
@@ -1,3 +1,6 @@
+"""Constants used throughout the training pipeline."""
+
+# Image token markers
 IMG_CONTEXT_TOKEN = '<IMG_CONTEXT>'
 IMG_START_TOKEN = '<img>'
 IMG_END_TOKEN = '</img>'
@@ -7,6 +10,8 @@ REF_START_TOKEN = '<ref>'
 REF_END_TOKEN = '</ref>'
 BOX_START_TOKEN = '<box>'
 BOX_END_TOKEN = '</box>'
+
+# Image normalization constants
 IMAGENET_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_STD = (0.229, 0.224, 0.225)
 CLIP_MEAN = (0.4814546, 0.4578275, 0.40821073)
